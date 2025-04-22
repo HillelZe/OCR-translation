fc330fa39f4c9ae91d52b7beefa46782831aab49
hillel zehavi <hillel.zehavi@gmail.com>
2025-04-22 11:25:52 +0200

    translation and sound work

--- camera_ocr.py
+++ camera_ocr.py
@@ -19,11 +19,46 @@ import cv2
 import pytesseract
 import numpy as np
 import time
+import requests
+import pyttsx3
 # pylint: disable = no-member
 
 
 def dist(p1, p2):
     return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+
+
+def translate(word):
+    api_key = "AIzaSyAPAfYDu7OIbFc4Am7-UNVICXzpk39tDuo"
+    url = "https://translation.googleapis.com/language/translate/v2"
+
+    params = {
+        "key": api_key,
+        "source": "fr",
+        "target": "en",
+        "q": word
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise error for bad HTTP codes
+        data = response.json()
+        translated_text = data["data"]["translations"][0]["translatedText"]
+        return translated_text  # or remove_nikkud(translated_text)
+    except requests.RequestException as e:
+        print("Error:", e)
+        raise
+
+
+def word_to_speak(word):
+    engine = pyttsx3.init()
+
+    # Set a common English voice directly (for Windows)
+    engine.setProperty(
+        'voice', 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Speech\\Voices\\Tokens\\TTS_MS_EN-US_DAVID_11.0')
+
+    engine.say(word)
+    engine.runAndWait()
 
 
 def empty(s):
@@ -60,12 +95,13 @@ class CameraOCR:
         - Press 'q' to quit the application and close the window.
         """
         distance_threshold = 3
-        time_still_treshold = 1.5
+        time_still_treshold = 1
         time_since_last_move = time.time()
         last_location = (0, 0)
         topmost = (0, 0)
         translated = True
         while True:
+            # print(f"top most:{topmost}")
             ret, frame = self.cap.read()
             if not ret:
                 print("Failed to grab frame.")
@@ -81,14 +117,19 @@ class CameraOCR:
 
             # Create a mask where blue is white and everything else is black
             mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
+            # cv2.imshow('mask', mask)
             contours, _ = cv2.findContours(
                 mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+            min_area = 10  # adjust this number as needed
+
             if contours:
                 largest_contour = max(contours, key=cv2.contourArea)
-                topmost = tuple(
-                    largest_contour[largest_contour[:, :, 1].argmin()][0])
+                if cv2.contourArea(largest_contour) >= min_area:
+                    topmost = tuple(
+                        largest_contour[largest_contour[:, :, 1].argmin()][0])
+                else:
+                    topmost = (0, 0)
             # check it the pen movement stoped
             # print(f"last location: {last_location}")
             # print(f"time since last move: {time.time()-time_since_last_move}")
@@ -96,7 +137,7 @@ class CameraOCR:
                 time_since_last_move = time.time()
                 last_location = topmost
                 translated = False
-            elif not translated and time.time()-time_since_last_move > time_still_treshold:
+            elif topmost != (0, 0) and not translated and time.time()-time_since_last_move > time_still_treshold:
 
                 image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -135,9 +176,13 @@ class CameraOCR:
                 print(f"word x:{word_x} word y:{word_y}")
                 print(f"pen x:{finger_x} pen y:{finger_y}")
                 print(choosen_word)
+                translation = translate(choosen_word)
+                print(translation)
+                word_to_speak(translation)
+
                 cv2.imshow('capture', frame)
                 translated = True
-
+                topmost = (0, 0)
             # Exit if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
